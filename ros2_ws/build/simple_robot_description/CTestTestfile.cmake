9635840fa8dfd2ba1236d03eb950fdebb1cdcefb
Somtochukwu <somtoosigwe1@gmail.com>
2026-05-28 18:37:36 +0100

    Refactor: Move ROS packages to repo root, remove nested ros2_ws/, add .gitignore

--- ros2_ws/build/simple_robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/somto/Documents/Graduate Degree Applications/Nottingham Trent University/3. Third Term 2026/Cognitive Robotics/cognitive_coursework/ros2_ws/src/simple_robot_description
-# Build directory: /mnt/c/Users/somto/Documents/Graduate Degree Applications/Nottingham Trent University/3. Third Term 2026/Cognitive Robotics/cognitive_coursework/ros2_ws/build/simple_robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
